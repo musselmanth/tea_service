098b328573249fbc761c899411ec26f5917bd3fa
Thomas Musselman <tmusselman@gmail.com>
2023-01-05 13:24:48 -0700

    refactor: subscriptions update to return serialized subscription

--- spec/requests/api/v1/subscriptions/update_spec.rb
+++ spec/requests/api/v1/subscriptions/update_spec.rb
@@ -18,8 +18,18 @@ RSpec.describe 'update subscription' do
         expect(subscription.status).to eq("active")
 
         patch "/api/v1/customers/#{customer.id}/subscriptions/#{subscription.id}", headers: headers, params: http_body.to_json
+        expect(response).to be_successful
+        expect(response).to have_http_status(202)
 
         expect(subscription.reload.status).to eq("cancelled")
+
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body).to have_key(:data)
+        expect(response_body[:data]).to be_a(Hash)
+        expect(response_body[:data]).to have_key(:id)
+        expect(response_body[:data]).to have_key(:type)
+        expect(response_body[:data]).to have_key(:attributes)
       end
     end
   end
